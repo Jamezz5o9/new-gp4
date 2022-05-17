f90ebe18f1a415a7f112cbe5e1f99f4fb727da7f
Aduloju James <adulojujames@gmail.com>
2022-05-17 01:53:44 +0100

    new addition

--- src/container/whatGPT3/WhatGPT3.jsx
+++ src/container/whatGPT3/WhatGPT3.jsx
@@ -4,7 +4,7 @@ import "./whatGPT3.css";
 
 const WhatGP3 = () => {
   return (
-    <div className="gpt3__whatgpt3 section__margin" id="whatgpt3">
+    <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
       <div className="gpt3__whatgpt3-feature">
         <Feature
           title="What is GPT-3"
